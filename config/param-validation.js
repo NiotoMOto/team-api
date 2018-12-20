c1e7ecef396379a21743fd668c5250dd3767abe9
Antoine Guillemoto <antoine.guillemoto@gmail.com>
2018-12-20 09:55:45 +0100

    fix(auth) add register validation schema

--- config/param-validation.js
+++ config/param-validation.js
@@ -5,22 +5,30 @@ module.exports = {
   createUser: {
     body: {
       username: Joi.string().required(),
-      password: Joi.string().required(),
+      password: Joi.string().required()
     }
   },
 
   // UPDATE /api/users/:userId
   updateUser: {
     body: {
-      username: Joi.string().required(),
+      username: Joi.string().required()
     },
     params: {
-      userId: Joi.string().hex().required()
+      userId: Joi.string()
+        .hex()
+        .required()
     }
   },
 
   // POST /api/auth/login
   login: {
+    body: {
+      username: Joi.string().required(),
+      password: Joi.string().required()
+    }
+  },
+  register: {
     body: {
       username: Joi.string().required(),
       password: Joi.string().required()
--- server/auth/auth.route.js
+++ server/auth/auth.route.js
@@ -8,16 +8,14 @@ const config = require('../../config/config');
 const router = express.Router(); // eslint-disable-line new-cap
 
 /** POST /api/auth/login - Returns token if correct username and password is provided */
-router.route('/login')
-  .post(validate(paramValidation.login), authCtrl.login);
+router.route('/login').post(validate(paramValidation.login), authCtrl.login);
 
-router.route('/register')
-  .post(validate(paramValidation.login), authCtrl.register);
-
+router.route('/register').post(validate(paramValidation.register), authCtrl.register);
 
 /** GET /api/auth/random-number - Protected route,
  * needs token returned by the above as header. Authorization: Bearer {token} */
-router.route('/random-number')
+router
+  .route('/random-number')
   .get(expressJwt({ secret: config.jwtSecret }), authCtrl.getRandomNumber);
 
 module.exports = router;
